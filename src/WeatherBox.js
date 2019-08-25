3828897b1b45420a32c284c157b9a86c8bb5bd19
kmorrical <kathryn.morrical@gmail.com>
2019-08-24 21:11:13 -0700

    cleanups after adding hooks/making functional components

--- src/WeatherBox.js
+++ src/WeatherBox.js
@@ -3,39 +3,31 @@ import './App.css';
 
 
 function WeatherBox(props) {
-    // constructor(props) {
-    //     super(props);
-
-
-    // };
     const { city, lowTemp, highTemp, currentTemp, humidity, conditions } = props;
-
 
     function temp(temper) {
         var newTemp = Math.round((temper - 273.15) * 9 / 5 + 32);
         return newTemp;
-    }
-
+    };
  
-        return (
-            <div className="container">
-      <div className="blueWeatherBox">
-      <br/>
-      <div className="weathertext">{city}</div>
-      <br/>
-      <div className="weathertext">{conditions}</div>
-      <br/>
-      <div className="weathertext currentTemp">{temp(currentTemp)}</div>
-      <br/>
-      <div className="weathertext">High Temperature: {temp(highTemp)}</div>
-      <br/>
-      <div className="weathertext"> Low Temperature: {temp(lowTemp)}</div>
-      <br/>
-      <div className="weathertext">Humidity: {humidity}</div>
-
-      </div>
+    return (
+      <div className="container">
+        <div className="blueWeatherBox">
+          <br/>
+          <div className="weathertext">{city}</div>
+          <br/>
+          <div className="weathertext">{conditions}</div>
+          <br/>
+          <div className="weathertext currentTemp">{temp(currentTemp)}</div>
+          <br/>
+          <div className="weathertext">High Temperature: {temp(highTemp)}</div>
+          <br/>
+          <div className="weathertext"> Low Temperature: {temp(lowTemp)}</div>
+          <br/>
+          <div className="weathertext">Humidity: {humidity}</div>
+        </div>
     </div>
         );
-
 }
+
 export default WeatherBox;
--- src/searchBar.js
+++ src/searchBar.js
@@ -3,34 +3,15 @@ import './App.css';
 
 
 function SearchBar(props) {
-    // constructor(props) {
-    //     super(props);
-
-    //     this.state = {
-    //         changedCity: ''
-    //     };
-    // };
     const { defaultCity, fetchCityData } = props;
-    // onInputChange = (event) => {
-    //     this.setState({ changedCity: event.target.value });
-    // }
-
     const [newCity, setCity] = useState(props.defaultCity);
-
-    // getCity = () => {
-    //     this.props.fetchCityData(this.state.changedCity);
-    // }
-
-    // render() {
         
-        return (
-            <div>
-
-    <input type="text" placeholder={defaultCity} onChange={(e) => setCity(e.target.value)}/>
-    <br/>
-    <button onClick={()=>fetchCityData(newCity)}>Enter</button>
-    </div>
+    return (
+        <div>
+            <input type="text" placeholder={defaultCity} onChange={(e) => setCity(e.target.value)}/>
+            <br/>
+            <button onClick={()=>fetchCityData(newCity)}>Enter</button>
+        </div>
         );
-    //}
 }
 export default SearchBar;
